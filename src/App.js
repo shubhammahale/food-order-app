45afcde2f5b6f1e0b7f06dc43697fd195c67d82d
Shubham Mahale1 <shubham.mahale1@ibm.com>
2021-11-18 02:03:03 +0530

    add cart functionality using context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartIsShwon, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <>
-      <Header></Header>
+    <CartProvider>
+      {cartIsShwon && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
